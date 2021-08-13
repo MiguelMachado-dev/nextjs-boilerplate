9be659395ee102333a96734c8cc26b0f0edbe1cd
Miguel Machado <miiguellmp@gmail.com>
2021-08-13 11:24:29 -0300

    feat(style): font-smoothing

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -17,6 +17,8 @@ const GlobalStyle = createGlobalStyle`
 
   body {
     font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `
 
